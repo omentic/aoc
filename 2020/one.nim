65e2321cbcec387dc881fddc5721cf6fe27f5e55
j-james <jj@j-james.me>
2020-11-30 22:11:08 -0800

    Day One: Produce clean output

--- 2020/one.nim
+++ 2020/one.nim
@@ -1,11 +1,14 @@
 import os, strutils
 
 let input = paramStr(1)
+var done: bool = false
 
 for one in lines(input):
   for two in lines(input):
-    if parseInt(one) + parseInt(two) == 2020:
+    if parseInt(one) + parseInt(two) == 2020 and not done:
       echo parseInt(one) * parseInt(two)
+      done = true
     for three in lines(input):
-      if parseInt(one) + parseInt(two) + parseInt(three) == 2020:
+      if parseInt(one) + parseInt(two) + parseInt(three) == 2020 and done:
         echo parseInt(one) * parseInt(two) * parseInt(three)
+        quit()
