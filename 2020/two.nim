7ffad964f3fab79b29f5705d1481a93613921a66
j-james <jj@j-james.me>
2020-12-02 22:29:55 -0800

    Day Two: Clean up code

--- 2020/two.nim
+++ 2020/two.nim
@@ -1,24 +1,23 @@
 import os, strutils
 
-let input = paramStr(1)
-var valid: int = 0
-var official: int = 0
+let input: string = paramStr(1)
+var unofficial, official: int = 0
 
 for line in lines(input):
   var frequency: int = 0
   let
     entry: seq[string] = split(line, {'-', ' ', ':'})
-    low: int = parseInt(entry[0])
-    high: int = parseInt(entry[1])
+    min: int = parseInt(entry[0])
+    max: int = parseInt(entry[1])
     letter: char = entry[2][0]
-    password: string = entry[4]
-  for char in password:
+    pass: string = entry[4]
+  for char in pass:
     if char == letter:
       inc(frequency)
-  if low <= frequency and frequency <= high:
-    inc(valid)
-  if password[low-1] == letter xor password[high-1] == letter:
+  if min <= frequency and frequency <= max:
+    inc(unofficial)
+  if pass[min-1] == letter xor pass[max-1] == letter:
     inc(official)
 
-echo valid
+echo unofficial
 echo official
