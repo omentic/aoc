38e25b7099338fd06af09bf37dbe2a2badb8d310
j-james <jj@j-james.me>
2020-12-04 23:31:24 -0800

    Day Five

---
+++ 2020/five.nim
@@ -0,0 +1,27 @@
+# Day Five: Binary Boarding
+import os, math, std/enumerate
+
+let input: string = paramStr(1)
+
+var
+  min: int = 2^8
+  max: int = 0
+  flight: seq[int]
+for seat in lines(input):
+  var row, column, id: int = 0
+  for i, char in enumerate(seat[0..^4]):
+    if char == 'B':
+      row += 2^(7-i) div 2
+  for i, char in enumerate(seat[7..^1]):
+    if char == 'R':
+      column += 2^(3-i) div 2
+  id = row * 8 + column
+  max = max(max, id)
+  min = min(min, id)
+  flight.add(id)
+
+echo max
+
+for id in min .. max:
+  if id notin flight:
+    echo id
