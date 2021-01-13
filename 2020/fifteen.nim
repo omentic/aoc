b4dcb59a8ace92d2a4343ad41a9230b727b1b850
j-james <jj@j-james.me>
2021-01-12 16:18:06 -0800

    Day Fifteen

---
+++ 2020/fifteen.nim
@@ -0,0 +1,20 @@
+# Day Fifteen: Rambunctious Recitation
+import os, strutils, sequtils
+
+let input: string = paramStr(1)
+
+proc recite(nth: int): int =
+  var numbers: seq[int] = map(split(strip(readFile(input)), ","), parseInt)
+  var history: seq[int] = newSeqWith[int](nth, -1)
+  for i in 0 .. nth - 2:
+    let previous: int = numbers[i]
+    if i == len(numbers) - 1:
+      if history[previous] != -1:
+        numbers.add(i - history[previous])
+      else:
+        numbers.add(0)
+    history[previous] = i
+  return numbers[^1]
+
+echo recite(2020)
+echo recite(30000000)
