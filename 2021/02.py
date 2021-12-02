4998c58a68c71101e63133bb5501b28be72093c7
j-james <jj@j-james.me>
2021-12-01 21:06:58 -0800

    Day Two

---
+++ 2021/02.py
@@ -0,0 +1,30 @@
+# Day Two: Dive!
+
+file = open("input/02.txt").readlines()
+
+hori = 0
+depth = 0
+
+for line in file:
+    com = line.split()
+    if com[0] == "forward":
+        hori += int(com[1])
+    if com[0] == "up":
+        depth -= int(com[1])
+    if com[0] == "down":
+        depth += int(com[1])
+print(hori * depth)
+
+hori = 0
+depth = 0
+aim = 0
+for line in file:
+    com = line.split()
+    if com[0] == "forward":
+        hori += int(com[1])
+        depth += aim * int(com[1])
+    if com[0] == "up":
+        aim -= int(com[1])
+    if com[0] == "down":
+        aim += int(com[1])
+print(hori * depth)
