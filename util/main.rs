6a3c52602974d411c99b312d9ae709e53ef0e29c
j-james <jj@j-james.me>
2022-11-30 22:46:43 -0800

    Delete submission scripts and update Rust template
    yeah i'm never going to use those scripts

--- util/main.rs
+++ util/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args = env::args().nth(1).expect("");
-    let input = fs::read_to_string(args).expect("");
+    let args = env::args().nth(1).unwrap();
+    let input = fs::read_to_string(args).unwrap();
     println!("{}", input);
 }
